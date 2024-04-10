ff9ff2521b0357fd8c53b575f46b03269179a894
hananmunir <hananmunir471@gmail.com>
2024-04-10 19:24:52 +0500

    added libaries for cities data

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import useFormStore from "@/utils/formStore";
 import { ArrowRight, ArrowLeft } from "lucide-react";
-
+import { Country, State, City } from "country-state-city";
 export default function Form() {
   const {
     step,
@@ -23,6 +23,7 @@ export default function Form() {
     resetForm,
   } = useFormStore();
 
+  console.log(City.getCitiesOfCountry("US"));
   const [calculatedPrice, setCalculatedPrice] = useState(null);
 
   const handleSubmit = (e) => {
@@ -93,66 +94,66 @@ export default function Form() {
   };
 
   return (
-    <div className="md:w-1/2 w-full bg-white p-6 rounded-lg h-full md:h-[100vh] md:overflow-auto flex flex-col justify-center items-center">
-      <h2 className="text-4xl font-semibold mb-6 text-black">Calculator</h2>
-      <form onSubmit={handleSubmit} className="w-full md:w-2/3">
+    <div className='md:w-1/2 w-full bg-white p-6 rounded-lg h-full md:h-[100vh] md:overflow-auto flex flex-col justify-center items-center'>
+      <h2 className='text-4xl font-semibold mb-6 text-black'>Calculator</h2>
+      <form onSubmit={handleSubmit} className='w-full md:w-2/3'>
         {step === 1 && (
           <>
-            <div className="mb-2">
-              <label htmlFor="tripName" className="block mb-1 text-black">
+            <div className='mb-2'>
+              <label htmlFor='tripName' className='block mb-1 text-black'>
                 Trip Name:
               </label>
               <input
-                type="text"
-                id="tripName"
+                type='text'
+                id='tripName'
                 value={tripName}
                 onChange={(e) => setTripName(e.target.value)}
-                placeholder="Enter trip name"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0"
+                placeholder='Enter trip name'
+                className='w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0'
                 style={{
                   color: "black",
                 }}
               />
             </div>
-            <div className="mb-2">
-              <label htmlFor="tripOrigin" className="block mb-1 text-black">
+            <div className='mb-2'>
+              <label htmlFor='tripOrigin' className='block mb-1 text-black'>
                 Trip Origin:
               </label>
               <input
-                type="text"
-                id="tripOrigin"
+                type='text'
+                id='tripOrigin'
                 value={tripOrigin}
                 onChange={(e) => setTripOrigin(e.target.value)}
-                placeholder="Enter trip origin"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0"
+                placeholder='Enter trip origin'
+                className='w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0'
                 style={{
                   color: "black",
                 }}
               />
             </div>
-            <div className="mb-2">
+            <div className='mb-2'>
               <label
-                htmlFor="tripDestination"
-                className="block mb-1 text-black"
+                htmlFor='tripDestination'
+                className='block mb-1 text-black'
               >
                 Trip Destination:
               </label>
               <input
-                type="text"
-                id="tripDestination"
+                type='text'
+                id='tripDestination'
                 value={tripDestination}
                 onChange={(e) => setTripDestination(e.target.value)}
-                placeholder="Enter trip destination"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0"
+                placeholder='Enter trip destination'
+                className='w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0'
                 style={{
                   color: "black",
                 }}
               />
             </div>
             <button
-              type="button"
+              type='button'
               onClick={nextStep}
-              className="bg-black text-white py-2 px-4 rounded hover:scale-105 focus:outline-none focus:ring-0 flex items-center justify-center transition-all duration-300 ease-in-out"
+              className='bg-black text-white py-2 px-4 rounded hover:scale-105 focus:outline-none focus:ring-0 flex items-center justify-center transition-all duration-300 ease-in-out'
             >
               <ArrowRight size={20} />
             </button>
@@ -160,81 +161,81 @@ export default function Form() {
         )}
         {step === 2 && (
           <>
-            <div className="mb-2">
-              <label htmlFor="length" className="block mb-1 text-black">
+            <div className='mb-2'>
+              <label htmlFor='length' className='block mb-1 text-black'>
                 Length (in cm):
               </label>
               <input
-                type="number"
-                id="length"
+                type='number'
+                id='length'
                 value={length}
                 onChange={(e) => setLength(e.target.value)}
-                placeholder="Enter length"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0"
+                placeholder='Enter length'
+                className='w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0'
                 style={{
                   color: "black",
                 }}
               />
             </div>
-            <div className="mb-2">
-              <label htmlFor="width" className="block mb-1 text-black">
+            <div className='mb-2'>
+              <label htmlFor='width' className='block mb-1 text-black'>
                 Width (in cm):
               </label>
               <input
-                type="number"
-                id="width"
+                type='number'
+                id='width'
                 value={width}
                 onChange={(e) => setWidth(e.target.value)}
-                placeholder="Enter width"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0"
+                placeholder='Enter width'
+                className='w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0'
                 style={{
                   color: "black",
                 }}
               />
             </div>
-            <div className="mb-2">
-              <label htmlFor="height" className="block mb-1 text-black">
+            <div className='mb-2'>
+              <label htmlFor='height' className='block mb-1 text-black'>
                 Height (in cm):
               </label>
               <input
-                type="number"
-                id="height"
+                type='number'
+                id='height'
                 value={height}
                 onChange={(e) => setHeight(e.target.value)}
-                placeholder="Enter height"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0"
+                placeholder='Enter height'
+                className='w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0'
                 style={{
                   color: "black",
                 }}
               />
             </div>
-            <div className="mb-2">
-              <label htmlFor="weight" className="block mb-1 text-black">
+            <div className='mb-2'>
+              <label htmlFor='weight' className='block mb-1 text-black'>
                 Weight (in kg):
               </label>
               <input
-                type="number"
-                id="weight"
+                type='number'
+                id='weight'
                 value={weight}
                 onChange={(e) => setWeight(e.target.value)}
-                placeholder="Enter weight"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0"
+                placeholder='Enter weight'
+                className='w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0'
                 style={{
                   color: "black",
                 }}
               />
             </div>
-            <div className="flex justify-between items-center">
+            <div className='flex justify-between items-center'>
               <button
-                type="button"
+                type='button'
                 onClick={prevStep}
-                className="bg-black text-white py-2 px-4 rounded hover:scale-105 focus:outline-none focus:ring-0 flex items-center justify-center transition-all duration-300 ease-in-out"
+                className='bg-black text-white py-2 px-4 rounded hover:scale-105 focus:outline-none focus:ring-0 flex items-center justify-center transition-all duration-300 ease-in-out'
               >
                 <ArrowLeft size={20} />
               </button>
               <button
-                type="submit"
-                className="bg-black text-white py-2 px-4 rounded hover:scale-105 focus:outline-none focus:ring-0 transition-all duration-300 ease-in-out"
+                type='submit'
+                className='bg-black text-white py-2 px-4 rounded hover:scale-105 focus:outline-none focus:ring-0 transition-all duration-300 ease-in-out'
               >
                 Calculate
               </button>
@@ -243,22 +244,22 @@ export default function Form() {
         )}
         {/* Reset button */}
         <button
-          type="button"
+          type='button'
           onClick={handleReset}
-          className="bg-gray-200 text-black py-2 px-4 rounded mt-4 focus:outline-none focus:ring-0 transition-all duration-300 ease-in-out"
+          className='bg-gray-200 text-black py-2 px-4 rounded mt-4 focus:outline-none focus:ring-0 transition-all duration-300 ease-in-out'
         >
           Reset Form
         </button>
       </form>
       {/* Display calculated price */}
       {calculatedPrice !== null && (
-        <p className="mt-4 text-black">
-          <span className="font-bold">Calculated Price: </span>$
+        <p className='mt-4 text-black'>
+          <span className='font-bold'>Calculated Price: </span>$
           {calculatedPrice.toFixed(2)}
         </p>
       )}
       {/* Step indicators */}
-      <div className="flex justify-center mt-4">
+      <div className='flex justify-center mt-4'>
         {[1, 2].map((s) => (
           <div
             key={s}
